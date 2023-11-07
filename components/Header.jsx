360110c42c56a03d7bd5fe0ced146068fff7e509
Ivy Chen <chenivy86085@gmail.com>
2023-11-06 21:35:57 -0600

    converting css and html to react

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header({name}) {
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+        <img src ="./images/pic.jpg" alt="Ivy's Profile Picture"/>
+
+    )
+}
